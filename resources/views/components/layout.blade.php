ff05ce631587df9b34d77b54266bb1a62ecd5231
balochCoder <raheelahmed017@gmail.com>
2024-03-22 15:43:14 +0400

    Final

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" class="h-full">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">   
+    <title>Code Breaker</title>
+    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,400,0,0" />
+</head>
+<body class="pt-8 bg-primary text-white print:bg-white print:text-black grid place-items-center h-full">
+    {{ $slot }}
+</body>
+</html>
